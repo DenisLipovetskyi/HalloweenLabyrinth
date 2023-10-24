86e22adb35cf7942b4e22814c935f508dce0f44e
denis <denislipovetskyi@gmail.com>
2023-10-24 00:11:19 +0000

    Game screen and WebView Navigation

--- app/src/main/java/com/example/halloweenlabyrinth/PrivacyPolicyScreen.kt
+++ app/src/main/java/com/example/halloweenlabyrinth/PrivacyPolicyScreen.kt
@@ -10,11 +10,10 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.filled.Info
 import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -31,6 +30,7 @@ import androidx.navigation.NavController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.example.halloweenlabyrinth.composable.WebViewScreen
 
 @Composable
 fun PrivacyPolicyScreen(navController: NavController) {
@@ -40,16 +40,12 @@ fun PrivacyPolicyScreen(navController: NavController) {
     val policyText = stringResource(id = R.string.privacy_policy)
 
     Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(16.dp),
+        modifier = Modifier.fillMaxSize().padding(16.dp),
         verticalArrangement = Arrangement.SpaceBetween
     ) {
         Text(
             text = policyText,
-            modifier = Modifier
-                .weight(1f)
-                .verticalScroll(rememberScrollState())
+            modifier = Modifier.weight(1f).verticalScroll(rememberScrollState())
         )
 
         Row(
@@ -68,9 +64,10 @@ fun PrivacyPolicyScreen(navController: NavController) {
 
             Button(onClick = {
                 with(sharedPreferences.edit()) {
-                    putBoolean("policy_accepted", false)
+                    putBoolean("policy_accepted", false) // Reset the accept flag
                     apply()
                 }
+                navController.navigate("game_screen_route")
             }) {
                 Text(text = "Deny")
             }
@@ -78,13 +75,13 @@ fun PrivacyPolicyScreen(navController: NavController) {
     }
 }
 
+
 @Composable
 fun AppNavigator() {
     val navController = rememberNavController()
     val context = LocalContext.current
     val sharedPreferences = context.getSharedPreferences("user_prefs", Context.MODE_PRIVATE)
     val isPolicyAccepted = sharedPreferences.getBoolean("policy_accepted", false)
-
 
     NavHost(navController = navController, startDestination = if (isPolicyAccepted) "game_screen_route" else "privacy_policy_route") {
         composable("privacy_policy_route") {
@@ -94,10 +91,31 @@ fun AppNavigator() {
             GameScreen(navController = navController)
         }
     }
+
+    NavHost(navController = navController, startDestination = /* ... */ ) {
+        composable("privacy_policy_route") {
+            PrivacyPolicyScreen(navController = navController)
+        }
+        composable("game_screen_route") {
+            GameScreen(navController = navController)
+        }
+        composable("webview_route/{link}") { backStackEntry ->
+            val link = backStackEntry.arguments?.getString("link") ?: ""
+            WebViewScreen(url = link)
+        }
+        composable("labyrinth_game_route") {
+            // Here, launch your Labyrinth game from the `com.example.halloweenlabyrinth` package
+            // Example:
+            // LabyrinthGame()   // Assuming you have a composable named LabyrinthGame in your game package.
+        }
+    }
 }
 
 @Composable
 fun GameScreen(navController: NavController) {
+
+
+
     Box(modifier = Modifier.fillMaxSize()) {
         // Background Image
         Image(
@@ -109,23 +127,36 @@ fun GameScreen(navController: NavController) {
 
         // Start Button in the center
         Button(
-            onClick = { /* Start Game Logic */ },
-            modifier = Modifier.align(Alignment.Center)
+            onClick = {
+                if (isPolicyAccepted) {
+                    // Fetch the server response
+                    val serverResponse = fetchServerResponse()  // Placeholder. Replace with actual API call.
+                    if (serverResponse.contains("http")) {  // A simple check. Refine based on actual response structure.
+                        navController.navigate("webview_route/${serverResponse}")
+                    } else {
+                        navController.navigate("labyrinth_game_route")
+                    }
+                } else {
+                    navController.navigate("labyrinth_game_route")
+                }
+            },
+            modifier = Modifier
+                .align(Alignment.BottomCenter)
+                .padding(50.dp)
         ) {
             Text(text = "Start Game")
         }
 
-        // Icon/Button to Read Privacy Policy Again at the bottom
+        // Button to Read Privacy Policy Again at the bottom
         IconButton(
             onClick = {
                 navController.navigate("privacy_policy_route")
             },
             modifier = Modifier
-                .align(Alignment.TopEnd)
-                .size(60.dp)
+                .align(Alignment.TopStart)
                 .padding(16.dp)
         ) {
-            Icon(imageVector = Icons.Default.Person, contentDescription = "Read Privacy Policy") // Use the appropriate icon
+            Icon(imageVector = Icons.Default.Info, contentDescription = "Read Privacy Policy") // Use the appropriate icon
         }
     }
 }
